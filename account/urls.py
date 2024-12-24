8292b53d3b1d27974dbda1cbabd7022078971782
maciej-PC <maciej.ko1444@gmail.com>
2024-12-24 14:43:34 +0100

    password reset, confirm added

--- account/urls.py
+++ account/urls.py
@@ -8,5 +8,13 @@ urlpatterns = [
     # login /logout urls
     path('login/', auth_views.LoginView.as_view(), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    
+    #reset password urls
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset-done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset_complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', views.dashboard, name='dashboard'),
 ]
