594b2b5e30838c80a0d232f3af36128bb3d16c67
maciej-PC <maciej.ko1444@gmail.com>
2024-12-27 11:29:15 +0100

    added intermediate model to build relationships between users

--- images/views.py
+++ images/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import get_object_or_404
 from .models import Image
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
 
 @login_required
 def image_create(request):
@@ -52,3 +54,32 @@ def image_like(request):
         except Image.DoesNotExist:
             pass
     return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # If AJAX request and page out of range
+            # return an empty page
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(
+            request,
+            'images/image/list_images.html',
+            {'section': 'images', 'images': images}
+        )
+    return render(
+        request,
+        'images/image/list.html',
+        {'section': 'images', 'images':images}
+    )
