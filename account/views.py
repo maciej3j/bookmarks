81804973bd4c55721e3c875dcd466fb7e0f02cf6
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-01-07 19:34:27 +0100

    Created active users list view and user detail view

--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import authenticate, get_user_model, login
 from django.http import HttpResponse
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
@@ -90,4 +90,21 @@ def edit(request):
             'profile_form': profile_form
         }
     )  
-    
+
+User = get_user_model()
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(request,
+                'account/user/list.html',
+                  {'section': 'people', 'users': users})
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    return render(
+        request,
+        'account/user/detail.html',
+        {'section': 'people', 'user': user}
+    )
