e43f6b78f5d275accb0cef0b010f2af50ad20cd4
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-01-08 11:12:29 +0100

    fix typo

--- account/views.py
+++ account/views.py
@@ -123,7 +123,7 @@ def user_follow(request):
                 Contact.objects.get_or_create(user_from=request.user,
                                               user_to=user)
             else:
-                Contact.objects.filter(user_from=request.user,usere_to=user).delete()
+                Contact.objects.filter(user_from=request.user,user_to=user).delete()
             return JsonResponse({'status': 'ok'})
         except User.DoesNotExist:
             return JsonResponse({'status': 'error'})
