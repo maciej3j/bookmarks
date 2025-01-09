4c06bc44fb8a32dd763a3ccf7a1860667908347e
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-01-09 18:07:34 +0100

    selecting prefetching related will vastly imrpove performance for retrieving objects from DB

--- account/views.py
+++ account/views.py
@@ -36,7 +36,7 @@ def dashboard(request):
     if following_ids:
         # If user is following others, retrieve only their actions
         actions = actions.filter(user_id__in=following_ids)
-    actions = actions[:10]
+    actions = actions.select_related('user', 'user__profile').prefetch_related('target')
     return render(
         request,
         'account/dashboard.html',
