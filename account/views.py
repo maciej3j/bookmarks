0e2e771c4955d144efd0f4e45cdbd07d398db235
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-01-09 17:58:58 +0100

    display an activity stream in the dashboard

--- account/views.py
+++ account/views.py
@@ -7,7 +7,7 @@ from .models import Contact, Profile
 from django.contrib import messages
 from django.views.decorators.http import require_POST
 from actions.utils import create_action
-
+from actions.models import Action
 
 def user_login(request):
     if request.method == "POST":
@@ -30,10 +30,17 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
+    # Display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions[:10]
     return render(
         request,
         'account/dashboard.html',
-        {'section': 'dashboard'}
+        {'section': 'dashboard', 'actions': actions}
     )
     
 def register(request):
