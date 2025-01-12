fa59640c45a78112da15d368cc74f2e058a36558
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-01-12 10:11:51 +0100

    fix bug

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -84,7 +84,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+] + PROJECT_APPS
 
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
