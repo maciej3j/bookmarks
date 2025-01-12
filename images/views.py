956295c9879b8714704f1c35ea0d83d69004ca03
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-01-12 10:03:59 +0100

    add redis and format with black formatter

--- images/views.py
+++ images/views.py
@@ -9,6 +9,14 @@ from django.views.decorators.http import require_POST
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import HttpResponse
 from actions.utils import create_action
+import redis
+from django.conf import settings
+
+
+r = redis.Redis(
+    host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB
+)
+
 
 @login_required
 def image_create(request):
@@ -20,51 +28,54 @@ def image_create(request):
             # assign current user to the item
             new_image.user = request.user
             new_image.save()
-            create_action(request.user, 'bookmarked image', new_image)
+            create_action(request.user, "bookmarked image", new_image)
             messages.success(request, "Image added successfully")
             # redirect to new created item detail view
             return redirect(new_image.get_absolute_url())
-    
+
     else:
         # build form with data provided by the bookmarklet via GET
         form = ImageCreateForm(data=request.GET)
     return render(
-        request,
-        'images/image/create.html',
-        {'section': 'images', 'form': form}
+        request, "images/image/create.html", {"section": "images", "form": form}
     )
-    
+
+
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f"image:{image.id}:views")
     return render(
-        request, 'images/image/detail.html', {'section': 'images', 'image': image}
+        request,
+        "images/image/detail.html",
+        {"section": "images", "image": image, "total_views": total_views},
     )
-    
+
+
 @login_required
 @require_POST
 def image_like(request):
-    image_id = request.POST.get('id')
-    action = request.POST.get('action')
+    image_id = request.POST.get("id")
+    action = request.POST.get("action")
     if image_id and action:
         try:
             image = Image.objects.get(id=image_id)
             if action == "like":
                 image.users_like.add(request.user)
-                create_action(request.user, 'likes', image)
+                create_action(request.user, "likes", image)
             else:
                 image.users_like.remove(request.user)
-            return JsonResponse({'status': 'ok'})
+            return JsonResponse({"status": "ok"})
         except Image.DoesNotExist:
             pass
-    return JsonResponse({'status': 'error'})
+    return JsonResponse({"status": "error"})
 
 
 @login_required
 def image_list(request):
     images = Image.objects.all()
     paginator = Paginator(images, 8)
-    page = request.GET.get('page')
-    images_only = request.GET.get('images_only')
+    page = request.GET.get("page")
+    images_only = request.GET.get("images_only")
     try:
         images = paginator.page(page)
     except PageNotAnInteger:
@@ -73,16 +84,14 @@ def image_list(request):
         if images_only:
             # If AJAX request and page out of range
             # return an empty page
-            return HttpResponse('')
+            return HttpResponse("")
         images = paginator.page(paginator.num_pages)
     if images_only:
         return render(
             request,
-            'images/image/list_images.html',
-            {'section': 'images', 'images': images}
+            "images/image/list_images.html",
+            {"section": "images", "images": images},
         )
     return render(
-        request,
-        'images/image/list.html',
-        {'section': 'images', 'images':images}
+        request, "images/image/list.html", {"section": "images", "images": images}
     )
