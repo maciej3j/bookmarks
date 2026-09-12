a5c95eaa65e02dcaf12cf6dd93ef46a3a3154817
Maciej Kowalski <maciej.ko1444@gmail.com>
2026-09-12 16:05:15 +0200

    add django_extensions to only debug mode

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+if DEBUG:
+    INSTALLED_APPS += "django_extensions"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
